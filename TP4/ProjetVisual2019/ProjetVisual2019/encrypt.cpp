749f43c0a94705fa5893c9d3e28c5850f94ae254
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2021-12-01 11:15:24 +0100

    TP4 Version Visual 2019

---
+++ TP4/ProjetVisual2019/ProjetVisual2019/encrypt.cpp
@@ -0,0 +1,66 @@
+#include "encrypt.h"
+
+namespace tp4
+{
+	Encrypt::Encrypt()
+	{
+
+	}
+
+	/*
+		Getter
+	*/
+
+	std::string Encrypt::plain() const
+	{
+		return _plain;
+	}
+
+	std::string Encrypt::cypher() const
+	{
+		return _cypher;
+	}
+
+	/*
+		Setter
+	*/
+
+	void Encrypt::updatePlain(std::string plain)
+	{
+		_plain = plain;
+	}
+
+	void Encrypt::updateCypher(std::string cypher)
+	{
+		_cypher = cypher;
+	}
+
+	/*
+		Helper
+	*/
+
+	std::string read(std::string address)
+	{
+		std::ifstream file(address);
+		std::string text = "";
+
+		if (file)
+		{
+			std::string line;
+			while (std::getline(file, line))
+				text += line;
+		}
+		else
+			std::cout << "Error: file not found" << std::endl;
+		return text;
+	}
+
+	void write(std::string address, std::string text)
+	{
+		std::ofstream file(address);
+		if (file)
+			file << text;
+		else
+			std::cout << "Error: file not created" << std::endl;
+	}
+}
---
+++ TP4/ProjetVisual2019/ProjetVisual2019/vigenere.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <array>
+#include <vector>
+#include "encrypt.h"
+
+namespace tp4 {
+	class Vigenere :
+		public Encrypt
+	{
+	protected:
+		std::array<int, 4> _key;
+
+	public:
+		Vigenere(std::array<int, 4> key);
+		std::array<int, 4> key();
+		void updateKey(std::array<int, 4> key);
+		void encode();
+		void decode();
+
+	};
+}
