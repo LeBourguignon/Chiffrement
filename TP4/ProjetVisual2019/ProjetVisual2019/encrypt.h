19146dbd47ee60dcd65716bf579cc833b6e62ff8
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-01-19 14:05:57 +0100

    Encrypt no virtual pur

--- TP4/ProjetVisual2019/ProjetVisual2019/encrypt.h
+++ TP4/ProjetVisual2019/ProjetVisual2019/encrypt.h
@@ -15,10 +15,22 @@ namespace tp4 {
 		std::string cypher() const;
 		void updatePlain(std::string plain);
 		void updateCypher(std::string cypher);
-		virtual void encode() = 0;
-		virtual void decode() = 0;
+		virtual void encode();
+		virtual void decode();
 	};
 
 	std::string read(std::string address);
 	void write(std::string address, std::string text);
 }
+
+/*
+	tp4::Encrypt encrypt;
+	encrypt.updatePlain(tp4::read("test.txt"));
+	std::cout << Text : << encrypt.plain() << std::endl;
+	encrypt.encode();
+	std::cout << Text encodé : << encrypt.cypher() << std::endl;
+	tp4::write("testEncode.txt", encrypt.cypher());
+	encrypt.decode();
+	std::cout << Text décodé : << encrypt.plain() << std::endl;
+	tp4::write("testDecode.txt", encrypt.cypher());
+*/
